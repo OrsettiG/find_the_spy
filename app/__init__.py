2f134da04d410702007758557a6fa1446f8cb5d7
OrsettiG <gjorsetti@gmail.com>
2020-01-17 22:24:34 -0800

    Production files

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func, select
 from sqlalchemy.orm import exc
-from flask_marshmallow import Marshmallow
 from datetime import datetime
 from config import Config
 import os
@@ -17,8 +16,5 @@ app.config.from_object(Config)
 
 # Init db
 db = SQLAlchemy(app)
-
-# Init marshmallow as ma - Maybe don't need this
-ma = Marshmallow(app)
 
 from app import views, models, options
--- app/views.py
+++ app/views.py
@@ -17,9 +17,6 @@ def index():
         except:
             return render_template('template.html')
             
-        # Drop all the old data from the last game (testing only)
-        # db.drop_all()
-        # db.create_all()
         
         # Create a new game and add it to the session
         try:
@@ -162,87 +159,3 @@ def reset():
     db.drop_all()
     db.create_all()
     return "db reset"
-
-
-
-
-# @app.route('/', methods=['POST'])
-# def add_players():
-#     if request.method == "POST" and request.form["add-btn"] == "Add Players":
-        
-#         # Load Vars with form data
-#         player_names = request.form["playerNames"].split(', ')
-#         print(player_names)
-#         num_players = len(player_names)
-        
-#         # Get current round from db
-#         r = Round.query.order_by(Round.id.desc()).first()
-#         print(r)
-        
-#         # Add the number of players in form data to players in current round 
-#         total_players = player_total(r.players, num_players)
-#         print(total_players)
-        
-#         # Update the round spy_count with correct number for the players
-#         r.spy_count = count_spies(total_players)
-        
-#         # Add new players to the session/round
-#         for name in player_names:
-#             p = new_Player(name, 0)
-#             session.add(p)
-#             r.players.append(p)
-            
-#         print(r.players)
-        
-#         # Commit session to db
-#         session.commit()
-        
-#         # Generate list of players and secrets (secret stays the same if players are added part way through round, but spies will change).
-#         players = generate_player_secrets(r)
-        
-#         # Pass player list to template.html
-#         return render_template('template.html', players=players)
-#     return render_template('template.html')
-   
-   
-# @app.route('/', methods=['POST', 'GET'])
-# def new_round():   
-#     if request.method == "POST" and request.form["new-btn"] == "New Round":
-        
-#         # Load vars with form and previous round data for easy handling
-#         player_names = request.form["playerNames"].split(', ')
-#         last_round = Round.query.order_by(Round.id.desc()).first()
-#         num_players = len(player_names) + len(last_round.players)
-#         secret_category = request.form["secretSelect"]
-        
-#         # Create a new round with appropriate spy_count and secret and add it to the session
-#         r = new_Round(num_players)
-#         session.add(r)
-        
-#         # Load round with randomized secret - will not repeat an already used secret
-#         r.secret = random_secret(secret_category)
-        
-#         # Get current Game, add it to the session and add Round to current it
-#         g = Game.query.order_by(Game.id.desc()).first()
-#         session.add(g)
-#         g.rounds.append(r)
-        
-#         # Add last round players to new round
-#         for player in last_round.players:
-#             r.players.append(player)
-        
-#         # Check if there are any new players to add to the round
-#         if len(player_names) > 0 and not player_names[0] == '' or player_names[0] == ' ':
-#             for name in player_names:
-#                 p = Player(name=name, win_count=0)
-#                 session.add(p)
-#                 r.players.append(p)
-        
-#         # Commit session to db
-#         session.commit()
-        
-#         # Generate list of players and secrets 
-#         players = generate_player_secrets(r)
-        
-#         return render_template('template.html', players=players)
-#     return render_template('template.html')
